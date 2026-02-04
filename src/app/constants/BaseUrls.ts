e7f379ad6085efb95b8e585c7e7bf7d63e43b40c
kesav2025 <kesavan.tht@gmail.com>
2026-02-04 10:03:31 +0530

    update

--- src/app/constants/BaseUrls.ts
+++ src/app/constants/BaseUrls.ts
@@ -1,10 +1,10 @@
 // export const baseUrl='https://www.fintastics.in/api/';
-export const socket_baseUrl='https://www.fintastics.in/fintastics';
+// export const socket_baseUrl='https://www.fintastics.in/fintastics';
 //live server
 
-export const baseUrl='http://192.168.1.3:3000/api/';
+// export const baseUrl='http://192.168.1.3:3000/api/';
 //local server
 
-// export const baseUrl='https://www.fintastics.ai/api/';
-// export const socket_baseUrl='https://www.fintastics.ai/fintastics';
+export const baseUrl='https://www.fintastics.ai/api/';
+export const socket_baseUrl='https://www.fintastics.ai/fintastics';
 
